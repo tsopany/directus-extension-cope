4968058171c94ffeeb86a00902bea1428181c0aa
Predrag Stojadinovic <predrags@nvidia.com>
2026-02-27 11:06:13 +0100

    chore: adding directus-extension-sync

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,6 @@ import type {HookConfig} from '@directus/extensions';
 
 import sqlAction from './actions/sql.action';
 import initAction from './actions/init.action'
-import exportAction from './actions/export.action';
-import importAction from './actions/import.action';
-
-const TABLES: string[] = [
-	// System Metadata
-	'directus_collections',
-	'directus_fields',
-	'directus_relations',
-
-	// Access Control
-	'directus_policies',
-	'directus_permissions',
-	'directus_roles',
-	'directus_access'
-];
 
 const registerHook: HookConfig = async ({init}: any, {database}: any): Promise<void> => {
 	init('cli.before', async ({program}: any): Promise<void> => {
@@ -36,18 +21,6 @@ const registerHook: HookConfig = async ({init}: any, {database}: any): Promise<v
 			.description('Execute raw SQL files in enums, tables, relations and triggers folders (including nested).')
 			.option('-d, --sql-dir <directory>', 'SQL directory path', './snapshots/sql/')
 			.action((options: { sqlDir: string }): Promise<never> => sqlAction(options, database));
-
-		cope
-			.command('export')
-			.description('Export Directus metadata to JSON file.')
-			.option('-f, --file <file>', 'Output file', './snapshots/cope-snapshot.json')
-			.action((options: { file: string }): Promise<never> => exportAction(TABLES, options, database));
-
-		cope
-			.command('import')
-			.description('Import Directus metadata from JSON file')
-			.option('-f, --file <file>', 'Input file', './snapshots/cope-snapshot.json')
-			.action((options: { file: string }): Promise<never> => importAction(TABLES, options, database));
 
 		cope.action((): void => cope.help());
 	});
